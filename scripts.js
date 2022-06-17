0d1118741b30735e9efaae7ee3851b9a86792416
Lucas <lucasfelicio378@gmail.com>
2022-06-17 15:12:00 -0300

    cards e geração aleatorias dos gifs

--- scripts.js
+++ scripts.js
@@ -1,20 +1,27 @@
 let valid = 0;
 let contador = 0;
-let  qtdCards;
+let qtdCards;
 while(valid == 0){
     qtdCards = Number(prompt('Com quantas cartas quer jogar? (apenas números pares entre 4 e 14)'));
     if(qtdCards % 2 == 0 && qtdCards >= 4 && qtdCards <=14 ) valid = 1;
 }  
 
-const gifs = [  'imgs/bobrossparrot.gif',
-                'imgs/explodyparrot.gif',
-                'imgs/fiestaparrot.gif',
-                'imgs/metalparrot.gif',
-                'imgs/revertitparrot.gif',
-                'imgs/tripletsparrot.gif',
-                'imgs/unicornparrot.gif' ];
-const gif_index = [6];
+let gifs = [    'img/bobrossparrot.gif',
+                'img/explodyparrot.gif',
+                'img/fiestaparrot.gif',
+                'img/metalparrot.gif',
+                'img/revertitparrot.gif',
+                'img/tripletsparrot.gif',
+                'img/unicornparrot.gif' ];
 
+let gifsAuxs = [];
+
+for (let index = 0; index < qtdCards; index++) {
+    gifsAuxs.push(gifs[index]);
+    gifsAuxs.push(gifs[index]);
+}
+
+gifsAuxs.sort(comparador);
 
 const cardsContainer = document.querySelector('.card_container');
 
@@ -22,23 +29,17 @@ function addCards(){
     for(let i = 0; i < qtdCards; i++){
         cardsContainer.innerHTML += 
             `<div class="card">
-                <div class="frente">
+                <div class="frente apagado">
                     <img src="./img/front.png" alt="">
                 </div>
 
-                <div class="fundo apagado" >
-                    <img src="./img/front.png" alt="">
+                <div class="fundo" >
+                    <img src="${gifsAuxs[i]}" alt="">
                 </div>
             </div>`;
     }
 }
 addCards();
-
-
-
-
-
-
 
 function comparador() { 
 	return Math.random() - 0.5; 
